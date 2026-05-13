645e80e866cf1cd6e15f91572462d7a35117d426
Jatin <jatinvaid00540@gmail.com>
2026-05-13 17:07:11 +0530

    Done till Functions

--- Basics/4.Functions.php
+++ Basics/4.Functions.php
@@ -1,1 +1,35 @@
 <?php
+function isArmstrong($num)
+{
+    $ogNum = $num;
+    $sum = 0;
+
+    $digits = strlen((string)$num);
+
+    while ($num > 0) {
+
+        $lastDigit = $num % 10;
+
+        $sum = $sum + ($lastDigit ** $digits);
+
+        $num = (int)($num / 10);
+    }
+
+    if ($sum == $ogNum) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+$input = (int) readline("Enter the Number: ");
+
+if (isArmstrong($input)) {
+    echo "$input is Armstrong";
+} else {
+    echo "$input is not Armstrong";
+}
+?>
+
+<!--C++ , Java are complied language constantly shows error while writing code like in Compile time-->
+<!--This is not the case with PHP It is a interpreted/executed script-->
